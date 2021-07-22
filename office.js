ba6826ce8d64a6e014f9d4aa94ee8d0c5015198b
Vikas <vikas.rg@somaiya.edu>
2021-07-22 14:55:19 +0530

    app.showNotification

--- office.js
+++ office.js
@@ -2,5 +2,11 @@ Office.onReady((info) => {
     if (info.host === Office.HostType.Word) {
         document.getElementById("sideload-msg").style.display = "none";
         document.getElementById("app-body").style.display = "flex";
+
     }
 });
+Office.initialize = function () {
+    window.alert = function (message) {
+        app.showNotification("Title For the Notification", message)
+    };
+};
