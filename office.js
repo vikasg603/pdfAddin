f4b84f23cba3a595c8059b75ff19b75e88ed53e8
Vikas <vikas.rg@somaiya.edu>
2021-07-22 14:52:15 +0530

    function not found

--- office.js
+++ office.js
@@ -2,6 +2,5 @@ Office.onReady((info) => {
     if (info.host === Office.HostType.Word) {
         document.getElementById("sideload-msg").style.display = "none";
         document.getElementById("app-body").style.display = "flex";
-        document.getElementById("run").onclick = getDocumentAsCompressed;
     }
 });
